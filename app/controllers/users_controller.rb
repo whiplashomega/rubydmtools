fd241cf8299277966d03a876fdeaeece123473f3
Joe Harrison <whiplashomega@gmail.com>
2015-03-26 20:00:48 +0000

    login functionality, tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "You have been signed up successfully to use the Dungeon Master's Toolset"
       redirect_to root_path
     else
