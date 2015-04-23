268ecbf171d0ad0280fd02f2856ca99516fa6757
Joe Harrison <whiplashomega@gmail.com>
2015-04-23 02:05:55 +0000

    journal controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327200118) do
+ActiveRecord::Schema.define(version: 20150423015827) do
 
   create_table "attacks", force: :cascade do |t|
     t.string   "name"
@@ -78,6 +78,13 @@ ActiveRecord::Schema.define(version: 20150327200118) do
   end
 
   add_index "characters", ["user_id"], name: "index_characters_on_user_id"
+
+  create_table "journals", force: :cascade do |t|
+    t.string   "date"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                      null: false
