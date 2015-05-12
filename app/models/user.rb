3f68d30462a6612bdce6af4f97f8656120119629
Joe Harrison <whiplashomega@gmail.com>
2015-05-12 20:17:32 +0000

    argh

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,19 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  
   private
+  
   def downcase_email
     self.email = email.downcase
   end
